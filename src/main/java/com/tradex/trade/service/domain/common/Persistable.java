bd05d03ede715cd5373d93ea422a93c9c6ca7498
pratek911 <prateek.work911@gmail.com>
2025-12-22 17:13:36 +0530

    domain:added domain models and entities for trade service tables

---
+++ src/main/java/com/tradex/trade/service/domain/common/Persistable.java
@@ -0,0 +1,28 @@
+package com.tradex.trade.service.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class Persistable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="created_at", nullable=false, updatable=false)
+    private String createdAt;
+
+    @Column(name="updated_at")
+    private String updatedAt;
+}
---
+++ src/main/java/com/tradex/trade/service/domain/entity/ProcessedEventEntity.java
@@ -0,0 +1,36 @@
+package com.tradex.trade.service.domain.entity;
+
+import com.tradex.trade.service.domain.common.Persistable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.Instant;
+
+@EnableJpaAuditing
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "processed_event",
+indexes = {
+        @Index(name = "idx_processedAt_id", columnList = "processed_at", unique = true)
+})
+public class ProcessedEventEntity extends Persistable {
+
+    @Column(name = "event_id", nullable = false)
+    private String eventId;
+
+    @Column(name = "processed_at", nullable = false)
+    private Instant processedAt = Instant.now();
+
+
+
+}
